9bc9aef2e6313fa19b49039942a9b5df3e11d06a
root <root@localhost.localdomain>
2020-02-28 17:22:27 +1100

    Site updated at 2020-02-28 06:22:27 UTC

--- downloads/code/getroot2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-int main()
-{
- setuid(0);
- system( "/bin/sh -i" );
-}
--- downloads/code/setuid.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-int main()
-{
- setuid(1000);
- system( "/bin/sh -i" );
-}
